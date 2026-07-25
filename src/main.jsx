a9884e884df36492707c1366fa5070a92e0b5cba
tudohhh <tudoramuraritei@yahoo.com>
2026-07-25 19:15:52 +0300

    FIX: corectat importul în main.jsx după restructurare

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Configurator from "./Configurator-v2.jsx";
+import Configurator from "./components/ui/Configurator.jsx";
 createRoot(document.getElementById("root")).render(<Configurator />);
