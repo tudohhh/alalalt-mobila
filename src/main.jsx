6343d5eced9a3b0e254db9c08a0d1614f2ec3b8f
tudohhh <tudoramuraritei@yahoo.com>
2026-07-25 23:29:30 +0300

    FORCE REBUILD: timestamp adăugat în main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,3 +2,5 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import Configurator from "./Configurator-v2.jsx";
 createRoot(document.getElementById("root")).render(<Configurator />);
+
+// FORCE REBUILD - 1785011370
